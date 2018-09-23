11d1e349d81bfa4e7bda23b11b1fe989678fe28a
Etay Meiri <etay.meiri@gmail.com>
2018-09-23 16:48:21 +0300

    1. changed the way achead:include-directories is defined. 2. added ac-source-yasnippet.

--- init.el
+++ init.el
@@ -127,14 +127,16 @@
   (require 'auto-complete-c-headers)
   (add-to-list 'ac-sources 'ac-source-c-headers)
   (add-to-list 'ac-sources 'ac-source-c-header-symbols t)
-  (add-to-list 'achead:include-directories '"/usr/include/c++/7
+  (add-to-list 'ac-sources 'ac-source-yasnippet)
+  (setq achead:include-directories
+	(append '("/usr/include/c++/7
  /usr/include/x86_64-linux-gnu/c++/7
  /usr/include/c++/7/backward
  /usr/lib/gcc/x86_64-linux-gnu/7/include
  /usr/local/include
  /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed
  /usr/include/x86_64-linux-gnu
-/usr/include"))
+/usr/include"))))
 
 (add-hook 'c++-mode-hook 'my:ac-c-header-init)
 (add-hook 'c-mode-hook 'my:ac-c-header-init)
