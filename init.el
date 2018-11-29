405ee3c1cc204c8078818af2854941a17050f5f4
emeiri <etay.meiri@intel.com>
2018-11-29 14:52:57 +0200

    Added AVY

--- init.el
+++ init.el
@@ -148,6 +148,14 @@ With a prefix argument, use comint-mode."
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 150 :family "Hack")))))
+
+
 (require 'ac-c-headers)
 (require 'auto-complete)
 (require 'auto-complete-config)
@@ -292,3 +300,7 @@ With a prefix argument, use comint-mode."
 
 (custom-set-variables
  '(helm-ag-ignore-buffer-patterns '("\\.caffe_pb2\\.py\\'")))
+
+(use-package avy
+  :ensure t
+  :bind ("M-s" . avy-goto-char))
