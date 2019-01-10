c099378b4a4d017e9ec21b145b5b9ae0f4966b82
emeiri <etay.meiri@intel.com>
2019-01-10 11:29:11 +0200

    Added org babel shell

--- init.el
+++ init.el
@@ -148,7 +148,7 @@ With a prefix argument, use comint-mode."
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*/caffe_pb2.py" "tags")))
  '(package-selected-packages
    (quote
-    (git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers))))
+    (babel git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers))))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;;(load-theme 'darkokai t)
@@ -203,12 +203,13 @@ With a prefix argument, use comint-mode."
        (string-match "compilation" (buffer-name buffer))
        (string-match "finished" string)
       )
-      (run-with-timer 1 nil
+      (run-with-timer 0.5 nil
                       (lambda (buf)
                        ; (bury-buffer buf)
 					; (switch-to-prev-buffer (get-buffer-window buf) 'kill)
 			(ace-window 1)
 			(delete-other-windows)
+			(message "Build OK")
 			)
                       buffer)))
 (add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
@@ -433,3 +434,7 @@ With a prefix argument, use comint-mode."
 (global-set-key (kbd "<f1>") 'list-buffers)
 (global-set-key (kbd "<f2>") 'helm-projectile-find-file-in-known-projects)
 (global-set-key (kbd "<f3>") 'helm-semantic-or-imenu)
+
+(require 'ob-shell)
+(org-babel-do-load-languages 'org-babel-load-languages '((sh . t )))
+
