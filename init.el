f9700db86be8e305359eb7314ee95fa3b7957b76
emeiri <etay.meiri@intel.com>
2019-01-21 13:33:56 +0200

    Added eyebrowse

--- init.el
+++ init.el
@@ -148,7 +148,7 @@ With a prefix argument, use comint-mode."
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*/caffe_pb2.py" "tags")))
  '(package-selected-packages
    (quote
-    (babel git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers))))
+    (eyebrowse babel git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers))))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;;(load-theme 'darkokai t)
@@ -430,10 +430,21 @@ With a prefix argument, use comint-mode."
    :ensure t
    :init (global-git-gutter-mode +1))
 
+(use-package eyebrowse
+  :ensure t
+  :config (eyebrowse-mode t))
+
+(defun switch-to-workflow()
+  (interactive)
+  (switch-to-buffer "workflow.org")
+  )
+
 ;; Global key bindings
 (global-set-key (kbd "<f1>") 'list-buffers)
 (global-set-key (kbd "<f2>") 'helm-projectile-find-file-in-known-projects)
 (global-set-key (kbd "<f3>") 'helm-semantic-or-imenu)
+(global-set-key (kbd "<f4>") 'switch-to-workflow)
+
 
 (require 'ob-shell)
 (org-babel-do-load-languages 'org-babel-load-languages '((sh . t )))
