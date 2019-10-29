d9d4334208337263eec7416f16ed315c19b57bd9
emeiri <etay.meiri@intel.com>
2019-10-29 15:17:00 +0200

    Refactor flags to init_flags.el

--- init.el
+++ init.el
@@ -1,49 +1,10 @@
 ;;;; General
 ; General
-;(set-background-color "black")
-;(cua-mode 1)
-(desktop-save-mode 1)                                      ;; save/restore opened files
-(electric-pair-mode 1)
-(global-auto-revert-mode 1)                                ;; when a file is updated outside emacs, make it update if it's already opened in emacs
-(global-hl-line-mode 1)                                    ;; turn on highlighting current line
-(global-linum-mode -1)                                      ;; show line numbers
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(set-face-attribute 'default nil :height 150)
-(setq auto-window-vscroll nil)
-(setq compilation-ask-about-save nil)
-(setq inhibit-startup-message t)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq visible-bell nil)
-(setq ring-bell-function 'ignore)
-(setq-default indent-tabs-mode -1)
-(show-paren-mode 1)                                        ;; turn on bracket match highlight
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(setq tooltip-use-echo-area t)
-(which-function-mode 1)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq next-line-add-newlines t)
-(winner-mode 1)
-(normal-erase-is-backspace-mode 0)
-(setq column-number-mode t)
-(savehist-mode 1)
-(size-indication-mode t)
-(set-language-environment "UTF-8")
-(electric-indent-mode 1)
-(setq scroll-conservatively 100)
-(set-frame-font "Hack 15" nil t)
-(setq create-lockfiles nil)
-(setq compilation-auto-jump-to-first-error t)
-(transient-mark-mode nil)
-(setq compilation-always-kill t)
+(load "~/.emacs.d/init_flags.el")
 
 (require 'misc)
 
 (add-hook 'prog-mode-hook (lambda () (hs-minor-mode 1)))
-
-(setq tramp-default-method "ssh")
 
 (defvar my-term-shell "/bin/bash")
 (defadvice ansi-term (before force-bash)
@@ -154,7 +115,7 @@ With a prefix argument, use comint-mode."
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*/caffe_pb2.py" "tags")))
  '(package-selected-packages
    (quote
-    (dumb-jump fill-function-arguments beacon undo-tree projectile helm yasnippet company helm-swoop swiper magit treemacs web-mode all-the-icons ztree move-text spaceline realgud dashboard smartscan imenu-anywhere free-keys iy-go-to-char company-anaconda company-quickhelp paredit flycheck-irony company-jedi irony-eldoc company-irony eyebrowse babel git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers)))
+    (helm-gtags dumb-jump fill-function-arguments beacon undo-tree projectile helm yasnippet company helm-swoop swiper magit treemacs web-mode all-the-icons ztree move-text spaceline realgud dashboard smartscan imenu-anywhere free-keys iy-go-to-char company-anaconda company-quickhelp paredit flycheck-irony company-jedi irony-eldoc company-irony eyebrowse babel git-gutter pcre2el dired+ treemacs-projectile smartparens ggtags expand-region hungry-delete jedi zenburn-theme flx-ido dash-functional yasnippet-snippets yasnippet-classic-snippets which-key use-package try tabbar-ruler solarized-theme org-bullets neotree magit-gh-pulls iedit helm-projectile helm-c-yasnippet helm-ag frame-tabs flycheck elpy doom-themes counsel autopair auto-complete-c-headers ag ace-window ac-c-headers)))
  '(send-mail-function (quote mailclient-send-it)))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
@@ -497,6 +458,12 @@ With a prefix argument, use comint-mode."
   :config
   (setq dumb-jump-selector 'helm))
 
+(use-package ztree
+  :ensure t)
+
+(use-package all-the-icons
+  :ensure t)
+
 (defun switch-to-workflow()
   (interactive)
   (switch-to-buffer "workflow.org")
@@ -702,11 +669,3 @@ might be bad."
 ;;(require 'ob-shell)
 (org-babel-do-load-languages 'org-babel-load-languages '((shell . t )))
 (setq org-confirm-babel-evaluate nil)
-
-(highlight-indentation-mode nil)
-
-(use-package ztree
-  :ensure t)
-
-(use-package all-the-icons
-  :ensure t)
