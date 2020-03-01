cf1114b60cba875e0c5057a12f984070a7aeadcb
emeiri <etay.meiri@intel.com>
2020-03-01 17:11:36 +0200

    fixed melpa links

--- init_melpa.el
+++ init_melpa.el
@@ -2,5 +2,6 @@
 (setq package-enable-at-startup nil)
 (setq package-archives '())
 ;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" .  "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" .  "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" .  "https://melpa.org/packages/") t)
 (package-initialize)
